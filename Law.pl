1a361b467310f7e7f4c5245ed80c28b59670599e
Phupa Denphatcharangkul <earthphupa@gmail.com>
2025-04-08 01:56:11 +0700

    changed death occurred

--- Law.pl
+++ Law.pl
@@ -15,7 +15,7 @@
 
     % Affray Case
     affray_participant/1,
-    death_occurred/0, prevented_affray/1,
+    death_occurred/1, prevented_affray/1,
 
     % Injury Cases
     harm/2, grievous_injury/2.
@@ -23,57 +23,50 @@
 % =========================================================
 % Handle Injury Case (Sections 295-298)
 % =========================================================
-% injury_case(Person, Victim, PersonAge, Grievous, Premeditated, Torture, CrimeRelated, VictimType)
-% CrimeRelated = Have a purpose of securing the benefit obtained through the other offence, or concealing the other offence or escaping punishment for the other offence committed by him
+% injury_case(Person, Victim, PersonAge, Injured, Intent, Grievous, Premeditated, Torture, CrimeRelated, VictimType)
+% CrimeRelated = purpose of benefiting from, concealing, or escaping another offense
 % VictimType can be:
 %   - ascendant
 %   - official
 %   - assistant
 %   - other
-handle_case(injury_case(Person, Victim, PersonAge, Grievous, Prem, Torture, CrimeRelated, VictimType)) :-
+handle_case(injury_case(Person, Victim, PersonAge, Injured, Intent, Grievous, Prem, Torture, CrimeRelated, VictimType)) :-
     assertz(harm(Person, Victim)),
     assertz(age(Person, PersonAge)),
+    ( Injured == true -> true ; true ),
+    assertz(intent(Person, Intent)),
     ( Grievous == true -> assertz(grievous_injury(Person, Victim)) ; true ),
     ( Prem == true -> assertz(premeditated(Person)) ; true ),
     ( Torture == true -> assertz(used_torture(Person)) ; true ),
     ( CrimeRelated == true -> assertz(murder_related_to_crime(Person)) ; true ),
     ( nonvar(VictimType) -> assertz(victim_type(Victim, VictimType)) ; true ).
 
-
 % =========================================================
 % Handle Murder Case (Sections 288-290)
 % =========================================================
-% murder_case(Person, Victim, PersonAge, Intent, Premeditated, Torture, CrimeRelated, VictimType)
-% CrimeRelated = Have a purpose of securing the benefit obtained through the other offence, or concealing the other offence or escaping punishment for the other offence committed by him
-% VictimType can be:
-%   - ascendant
-%   - official
-%   - assistant
-%   - other
-handle_case(murder_case(Person, Victim, PersonAge, Intent, Prem, Torture, CrimeRelated, VictimType)) :-
+handle_case(murder_case(Person, Victim, PersonAge, Intent, Prem, Torture, CrimeRelated, VictimType, Death)) :-
     assertz(murder(Person, Victim)),
     assertz(age(Person, PersonAge)),
     assertz(intent(Person, Intent)),
     ( Prem == true -> assertz(premeditated(Person)) ; true ),
     ( Torture == true -> assertz(used_torture(Person)) ; true ),
     ( CrimeRelated == true -> assertz(murder_related_to_crime(Person)) ; true ),
-    ( nonvar(VictimType) -> assertz(victim_type(Victim, VictimType)) ; true ).
+    ( nonvar(VictimType) -> assertz(victim_type(Victim, VictimType)) ; true ),
+    ( Death == true -> assertz(death_occurred(murder)) ; true ).
 
 % =========================================================
 % Handle Negligent Case (Section 291, 300)
 % =========================================================
-% negligent_case(Person, Victim, PersonAge, Circumstance, GrievousInjury, CausedDeath)
 handle_case(negligent_case(Person, Victim, PersonAge, Circumstance, Grievous, Death)) :-
     assertz(negligent_act(Person, Victim)),
     assertz(age(Person, PersonAge)),
     assertz(circumstance(Person, Circumstance)),
     ( Grievous == true -> assertz(grievous_injury(Person, Victim)) ; true ),
-    ( Death == true -> assertz(death_occurred) ; true ).
+    ( Death == true -> assertz(death_occurred(negligence)) ; true ).
 
 % =========================================================
 % Handle Suicide Case by Cruelty (Section 292)
 % =========================================================
-% suicide_cruelty_case(Person, Victim, PersonAge, VictimAge, SuicideOccurred, Dependent, UsedCruelty)
 handle_case(suicide_cruelty_case(Person, Victim, PersonAge, VictimAge, Occurred, Dependent, UsedCruelty)) :-
     assertz(age(Person, PersonAge)),
     assertz(age(Victim, VictimAge)),
@@ -84,11 +77,6 @@ handle_case(suicide_cruelty_case(Person, Victim, PersonAge, VictimAge, Occurred,
 % =========================================================
 % Handle Suicide Case by Aiding or Instigation (Section 293)
 % =========================================================
-% suicide_aid_case(Person, Victim, PersonAge, VictimAge, SuicideOccurred, VictimType)
-% VictimType can be:
-%   - child
-%   - incompetent
-%   - uncontrollable
 handle_case(suicide_aid_case(Person, Victim, PersonAge, VictimAge, Occurred, VictimType)) :-
     assertz(age(Person, PersonAge)),
     assertz(age(Victim, VictimAge)),
@@ -99,16 +87,15 @@ handle_case(suicide_aid_case(Person, Victim, PersonAge, VictimAge, Occurred, Vic
 % =========================================================
 % Handle Group Affray Case (Section 294, 299)
 % =========================================================
-% affray_case(Person, PersonAge, DeathOccurred, PreventedAffray, GrievousInjury)
 handle_case(affray_case(Person, PersonAge, Death, Prevented, Grievous)) :-
     assertz(age(Person, PersonAge)),
     assertz(affray_participant(Person)),
-    ( Death == true -> assertz(death_occurred) ; true ),
+    ( Death == true -> assertz(death_occurred(affray)) ; true ),
     ( Prevented == true -> assertz(prevented_affray(Person)) ; true ),
     ( Grievous == true -> assertz(grievous_injury(Person, victim)) ; true ).
 
 % =========================================================
-% Sentencing Rules (Sections 288-294)
+% Sentencing Rules (Sections 288-299)
 % =========================================================
 
 % --------- SECTION 295: Basic Bodily Harm ---------
@@ -164,6 +151,20 @@ sentence(Person, '2 to 10 years imprisonment') :-
 sentence(Person, death) :-
     murder(Person, Victim),
     intent(Person, true),
+    death_occurred(murder),
+    (
+        premeditated(Person)
+        ; used_torture(Person)
+        ; murder_related_to_crime(Person)
+        ; victim_type(Victim, ascendant)
+        ; victim_type(Victim, official)
+        ; victim_type(Victim, assistant)
+    ), !.
+
+sentence(Person, 'Life time imprisonment (attempted)') :-
+    murder(Person, Victim),
+    intent(Person, true),
+    \+ death_occurred(murder),
     (
         premeditated(Person)
         ; used_torture(Person)
@@ -176,9 +177,15 @@ sentence(Person, death) :-
 % --------- SECTION 288: MURDER WITHOUT AGGRAVATION ---------
 sentence(Person, '15-20 years imprisonment') :-
     murder(Person, _),
-    intent(Person, true), !.
+    intent(Person, true),
+    death_occurred(murder), !.
 
-% --------- SECTION 290: UNINTENTIONAL KILLING (INJURY CAUSED DEATH) ---------
+sentence(Person, '10-13 years imprisonment (attempted)') :-
+    murder(Person, _),
+    intent(Person, true),
+    \+ death_occurred(murder), !.
+
+% --------- SECTION 290: UNINTENTIONAL KILLING ---------
 sentence(Person, '3-20 years imprisonment (aggravated injury)') :-
     murder(Person, Victim),
     intent(Person, false),
@@ -199,12 +206,12 @@ sentence(Person, '3-15 years imprisonment') :-
 sentence(Person, 'up to 3 years or 6,000 Baht fine or both') :-
     negligent_act(Person, Victim),
     grievous_injury(Person, Victim),
-    \+ death_occurred, !.
+    \+ death_occurred(negligence), !.
 
 % --------- SECTION 291: Negligent Causing Death ---------
 sentence(Person, 'not more than 10 years imprisonment or 20,000 Baht fine') :-
     negligent_act(Person, _),
-    death_occurred,
+    death_occurred(negligence),
     \+ grievous_injury(Person, _), !.
 
 % --------- SECTION 292: Suicide due to cruelty toward dependent ---------
@@ -229,12 +236,12 @@ sentence(Person, none) :-
 
 sentence(Person, 'up to 2 years or 4,000 Baht fine or both') :-
     affray_participant(Person),
-    death_occurred, !.
+    death_occurred(affray), !.
 
 % --------- SECTION 299: Affray Causing Grievous Injury ---------
 sentence(Person, 'up to 1 year or 2,000 Baht fine or both') :-
     affray_participant(Person),
-    \+ death_occurred,
+    \+ death_occurred(affray),
     grievous_injury(_, _),
     \+ prevented_affray(Person), !.
 
@@ -257,6 +264,6 @@ clear_case :-
     retractall(used_cruelty(_)),
     retractall(affray_participant(_)),
     retractall(prevented_affray(_)),
-    retractall(death_occurred),
+    retractall(death_occurred(_)),
     retractall(harm(_, _)),
     retractall(grievous_injury(_, _)).
