7c701731f23bfccdcfbe375a31ec4a0397f4121d
Phupa Denphatcharangkul <earthphupa@gmail.com>
2025-04-09 01:34:43 +0700

    update dataset again hopefullly for the last time

--- test_intent_classifier.py
+++ test_intent_classifier.py
@@ -18,7 +18,7 @@ classifier.label_encoder.fit([item["intent"] for item in dataset])  # Fit the la
 # Load model for inference
 inference_classifier = classifier.load_model_for_inference()
 
-trained_texts = ["My friend punch a guy in a bar 2 days ago what will happen to him", "What if I call my friends fat lead to him commited suicide will I be punished?", "How much salt do I put on the steak?", "What sentence do woman get if she does abortion?", "what happens if i punched John", "what happen if i left my child", "john inflicted heavy injury to a guy"]
+trained_texts = ["What will happens if I injured someone accidentally",]
 for sample_text in trained_texts:
     predicted_intent, confidence = classifier.predict_intent(sample_text, inference_classifier)
     print(f"Predicted intent for '{sample_text}': {predicted_intent} , Confidence: {confidence:.2f}")
